7745be90cf3b9d54800b1323730e95bfab30aef4
reshetnikd <dmitry.reshetnik@icloud.com>
2020-06-10 17:02:10 +0300

    Add alert with a text field to insert a line of text for the meme

--- MemeGenerator/ViewController.swift
+++ MemeGenerator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topTextButton: UIButton!
+    @IBOutlet weak var bottomTextButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,14 +31,71 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         }
         
         imageView.image = image
-        
+        topTextButton.isEnabled = false
+        bottomTextButton.isEnabled = false
         navigationItem.leftBarButtonItem?.isEnabled = false
     }
 
     @IBAction func setTopText(_ sender: UIButton) {
+        let ac = UIAlertController(title: "Enter top text", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned ac] _ in
+            let answer = ac.textFields![0]
+            let renderer = UIGraphicsImageRenderer(size: CGSize(width: self.imageView.image!.size.width, height: self.imageView.image!.size.height + 50))
+            let image = renderer.image { ctx in
+                let paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.alignment = .center
+
+                let attributes = [
+                    NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Thin", size: 36)!,
+                    NSAttributedString.Key.paragraphStyle: paragraphStyle,
+                    NSAttributedString.Key.backgroundColor: UIColor.black,
+                    NSAttributedString.Key.foregroundColor: UIColor.white
+                ]
+
+                let string = answer.text!
+                string.draw(with: CGRect(x: 0, y: 0, width: self.imageView.image!.size.width, height: 50), options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+                self.imageView.image!.draw(at: CGPoint(x: 0, y: 50))
+            }
+            
+            self.imageView.image = image
+        }
+
+        ac.addAction(submitAction)
+
+        present(ac, animated: true)
     }
     
     @IBAction func setBottomText(_ sender: UIButton) {
+        let ac = UIAlertController(title: "Enter top text", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [unowned ac] _ in
+            let answer = ac.textFields![0]
+            let renderer = UIGraphicsImageRenderer(size: CGSize(width: self.imageView.image!.size.width, height: self.imageView.image!.size.height + 50))
+            let image = renderer.image { ctx in
+                let paragraphStyle = NSMutableParagraphStyle()
+                paragraphStyle.alignment = .center
+
+                let attributes = [
+                    NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Thin", size: 36)!,
+                    NSAttributedString.Key.paragraphStyle: paragraphStyle,
+                    NSAttributedString.Key.backgroundColor: UIColor.black,
+                    NSAttributedString.Key.foregroundColor: UIColor.white
+                ]
+
+                let string = answer.text!
+                string.draw(with: CGRect(x: 0, y: self.imageView.image!.size.height, width: self.imageView.image!.size.width, height: 50), options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+                self.imageView.image!.draw(at: CGPoint(x: 0, y: 0))
+            }
+            
+            self.imageView.image = image
+        }
+
+        ac.addAction(submitAction)
+
+        present(ac, animated: true)
     }
     
     @objc func importImage() {
@@ -61,6 +120,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         guard let image = info[.editedImage] as? UIImage else { return }
         
         imageView.image = image
+        topTextButton.isEnabled = true
+        bottomTextButton.isEnabled = true
         navigationItem.leftBarButtonItem?.isEnabled = true
         
         dismiss(animated: true)
